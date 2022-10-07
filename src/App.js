6d5b663b7c3906f98b149257bce0aef7a1a9c9ac
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-10-07 00:08:55 +0000

    Got proof of life with server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import UserInput from './UserInput'
 import io from 'socket.io-client'
 
-const URL = process.env.URL || 'http://localhost:3001/'
-const socket = io(URL);
+const URL = process.env.URL || 'http://localhost:3002/messages'
+const socket = io.connect(URL);
 function App() {
 socket.emit('join', {clientId: socket.id})
+
+console.log(socket.id)
   return (<>
     <h1>hello world</h1>
     <UserInput/>
